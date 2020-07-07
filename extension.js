4a22d8aa1b083a4d4c3b9efb003f48c1a1345970
John Colagioia <john@colagioia.net>
2020-07-07 07:40:33 -0400

    Update extension to send updates to a remote server

--- extension.js
+++ extension.js
@@ -1,3 +1,4 @@
+var http = require('http');
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
@@ -13,18 +14,44 @@ function activate(context) {
   
   vscode.workspace.onDidSaveTextDocument((document) => {
     const file = document.fileName;
-    vscode.window.showInformationMessage(`Saved ${file}`);
+    const post_data = `Saved ${file}`;
+
+    httpPost({
+      body: post_data,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      hostname: 'localhost',
+      path: '/'
+    });
   });
 
   vscode.workspace.onDidOpenTextDocument((document) => {
     const file = document.fileName;
-    vscode.window.showInformationMessage(`Opened ${file}`);
+    const post_data = `Opened ${file}`;
+
+    httpPost({
+      body: post_data,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      hostname: 'localhost',
+      path: '/'
+    });
   });
 
   vscode.workspace.onDidChangeTextDocument((changes) => {
     const file = changes.document.fileName;
-    console.log(JSON.stringify(changes));
-    vscode.window.showInformationMessage(`Modified ${file}`);
+    const post_data = `Modified ${file}\n${JSON.stringify(changes.contentChanges)}`;
+
+    httpPost({
+      body: post_data,
+      headers: {
+        'Content-Type': 'text/plain',
+      },
+      hostname: 'localhost',
+      path: '/'
+    });
   });
 }
 exports.activate = activate;
@@ -35,4 +62,30 @@ function deactivate() {}
 module.exports = {
 	activate,
 	deactivate
+}
+
+function httpPost({body, ...options}) {
+  return new Promise((resolve,reject) => {
+      const req = http.request({
+          method: 'POST',
+          ...options,
+      }, res => {
+          const chunks = [];
+          res.on('data', data => chunks.push(data))
+          res.on('end', () => {
+              let body = Buffer.concat(chunks);
+              switch(res.headers['content-type']) {
+                  case 'application/json':
+                      body = JSON.parse(body.toString());
+                      break;
+              }
+              resolve(body)
+          })
+      })
+      req.on('error',reject);
+      if(body) {
+          req.write(body);
+      }
+      req.end();
+  })
 }
